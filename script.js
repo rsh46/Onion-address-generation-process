57021820d82135b68c012ccb26393263548671db
rsh46 <167907073+rsh46@users.noreply.github.com>
2024-07-30 21:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,31 @@
-const ec = new elliptic.ec('ed25519');
 
-function generateKeypair() {
-    const key = ec.genKeyPair();
-    return {
-        privateKey: key.getPrivate('hex'),
-        publicKey: key.getPublic('hex')
-    };
+const generateBtn = document.getElementById('generate-btn');
+const resultDiv = document.getElementById('result');
+const patternInput = document.getElementById('pattern');
+
+let worker;
+
+generateBtn.addEventListener('click', async () => {
+	const pattern = patternInput.value;
+	if (!worker) {
+		worker = new Worker('worker.js');
+	}
+
+	worker.postMessage({ pattern });
+	worker.onmessage = (event) => {
+		const { privateKey, publicKey, onionAddress } = event.data;
+		resultDiv.innerHTML = `
+			<p>Private Key:</p>
+			<pre>${base64Encode(privateKey)}</pre>
+			<p>Public Key:</p>
+			<pre>${base64Encode(publicKey)}</pre>
+			<p>Onion Address:</p>
+			<pre>${onionAddress}</pre>
+		`;
+	};
+});
+
+function base64Encode(buffer) {
+	return btoa(String.fromCharCode(...new Uint8Array(buffer)));
 }
-
-function getOnionAddress(publicKey) {
-    const pubBytes = hexToBytes(publicKey);
-    const checksum = ".onion checksum";
-    const version = new Uint8Array([0x03]);
-    const combined = new Uint8Array(pubBytes.length + checksum.length + version.length);
-    combined.set(pubBytes);
-    combined.set(new TextEncoder().encode(checksum), pubBytes.length);
-    combined.set(version, pubBytes.length + checksum.length);
-    return base32Encode(combined).toLowerCase().slice(0, 56);
-}
-
-function hexToBytes(hex) {
-    const bytes = new Uint8Array(hex.length / 2);
-    for (let i = 0; i < hex.length; i += 2) {
-        bytes[i / 2] = parseInt(hex.substr(i, 2), 16);
-    }
-    return bytes;
-}
-
-function base32Encode(input) {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-    let output = '';
-    let bits = 0;
-    let value = 0;
-
-    for (let i = 0; i < input.length; i++) {
-        value = (value << 8) | input[i];
-        bits += 8;
-        while (bits >= 5) {
-            output += alphabet[(value >>> (bits - 5)) & 31];
-            bits -= 5;
-        }
-    }
-    if (bits > 0) {
-        output += alphabet[(value << (5 - bits)) & 31];
-    }
-    return output;
-}
-
-function generateVanityAddress() {
-    const prefix = document.getElementById('prefix').value.toLowerCase();
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = 'Generating... This may take a while.';
-
-    const worker = new Worker(URL.createObjectURL(new Blob([`
-        ${generateKeypair.toString()}
-        ${getOnionAddress.toString()}
-        ${hexToBytes.toString()}
-        ${base32Encode.toString()}
-        
-        onmessage = function(e) {
-            const prefix = e.data;
-            while (true) {
-                const { privateKey, publicKey } = generateKeypair();
-                const address = getOnionAddress(publicKey);
-                if (address.startsWith(prefix)) {
-                    postMessage({ privateKey, publicKey, address });
-                    break;
-                }
-            }
-        };
-    `], {type: 'text/javascript'})));
-
-    worker.onmessage = function(e) {
-        const { privateKey, publicKey, address } = e.data;
-        resultElement.innerHTML = `
-            Found vanity address: ${address}<br><br>
-            Private key: ${privateKey}<br><br>
-            Public key: ${publicKey}
-        `;
-        worker.terminate();
-    };
-
-    worker.postMessage(prefix);
-}
+  
